3be667a7db1daf0464639e87f6c8776f755e0757
Humberto <hum.alex93@gmail.com>
2024-09-09 14:28:08 -0600

    Django view function html page

--- app/views.py
+++ app/views.py
@@ -1,56 +1,20 @@
 from django.shortcuts import render
 from app.models import GeneralInfo
-from django.db.models import Q
-from django.db import connection
-
-def writes_sql_queries_to_file(file_path):
-  with open(file_path,'w') as file:
-    queries = connection.queries
-    for query in queries:
-      sql = query['sql'] 
-      file.write(f"{sql}\n")
 
 # Create your views here.
 def index(request):
+  general_info = GeneralInfo.objects.first()
 
-  # #READ
-  # all_records = GeneralInfo.objects.all()
-  # print(all_records)
-  
-  # first_record = GeneralInfo.objects.first()
-  # print(first_record)
+  contex = {
+    "company_name": general_info.company_name,
+    "location": general_info.location,
+    "email": general_info.email,
+    "phone": general_info.phone,
+    "open_hours": general_info.open_hours,
+    "video_url": general_info.video_url,
+    "facebook_url": general_info.facebook_url,
+    "instagram_url": general_info.instagram_url,
+    "linkedin_url": general_info.linkedin_url,
+  }
 
-  # last_five_record = GeneralInfo.objects.all().order_by('-id')[:5]
-  # for i in last_five_record:
-  #   print(i.location)
-
-
-  # #DELETE
-  # GeneralInfo.objects.filter(company_name="Company").delete()
-
-  # #UPDATE
-  # company = GeneralInfo.objects.get(id=1)
-  # company.phone = "56145677"
-  # company.save()
-
-  # company = GeneralInfo.objects.all().update(phone = " 741585585")
-
-  # #CREATE
-  # GeneralInfo.objects.create (
-  #   company_name = "Samsung",
-  #   location= "asia",
-  #   phone = "8528525",
-  #   email = "fghj@example.com"
-
-  # )
-
-
-
-  file_path = 'sql_queries.txt'
-  writes_sql_queries_to_file(file_path)
-
-  context = {}
-
-
-  return render(request,"index.html",context)
-
+  return render (request, "index.html",contex) 
